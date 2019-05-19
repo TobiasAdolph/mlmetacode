d4fa96f6dc7c11cfd8628a2b5df22f28e4f68f16
Tobias Weber <weber@lrz.de>
2019-05-19 21:21:13 +0000

    Onsite changes (lb-0)

--- code/retrieve/retrieveHelpers.py
+++ code/retrieve/retrieveHelpers.py
@@ -8,15 +8,15 @@ import subprocess
 import sys
 import time
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('retrieve')
 logger.setLevel(logging.DEBUG)
-
 fh = logging.FileHandler('retrieve.log')
 fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(levelname)s|%(asctime)s -- %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
@@ -36,23 +36,43 @@ def doHarvest(payload):
         raise Exception("ERROR")
     try:
         hv = config["hvs"][hvIdx]
-        logger.debug("using {}".format(hv))
         hvConfig = loadHvConfig(fileName)
+        logger.info("using {} for {} with url and unload to {}".format(
+            hv,
+            fileName,
+            config["hvUnloadSrc"][hvIdx]))
+
+        logger.info("Loading harvester {} for {} with hvconfig {}".format(
+            hv,
+            fileName,
+            hvConfig["OaiPmhETL.hostURL"]))
         loadHarvester(hvConfig, hv, config)
         startHarvester(hv)
         hvState = "HARVESTING"
         while hvState in ["HARVESTING", "QUEUED"]:
             time.sleep(config["sleep"])
-            logger.info("slept {} seconds, checking harvester".format(config["sleep"]))
-            hvState = checkHarvester(hv)
-        logger.info("Harvester finished with state {}".format(hvState))
+            logger.debug("Harvester {} for {} slept {} seconds, checking harvester".format(
+                hv,
+                fileName,
+                config["sleep"]))
+            (hvState, hvHealth) = checkHarvester(hv)
+        logger.info("Harvester {} for {} finished with state {} and health {}".format(
+            hv,
+            fileName,
+            hvState,
+            hvHealth)
+        )
         returnHarvester(hvIdx)
         if hvState == "IDLE":
             time.sleep(5)
             unloadHarvester(hvIdx, config, target)
             return True
         else:
-            logger.error("Harvester finished with status code {}".format(hvState))
+            logger.error("Harvester {} for {} finished with status code {}".format(
+                hv,
+                fileName,
+                hvState)
+            )
             return False
 
     except Exception as e:
@@ -61,24 +81,33 @@ def doHarvest(payload):
         return False
 
 def loadHarvester(hvConfig, hv, config):
-    logger.info("Loading harvester {} with hvconfig {}".format(hv, hvConfig["OaiPmhETL.hostURL"]))
     headers = {'Content-Type': "application/json"}
     hvUrl = "{}/config/_set".format(hv)
-    hvConfig["OaiPmhETL.rangeTo"] = config["hvRangeTo"]
+    if "OaiPmhETL.rangeTo" not in hvConfig.keys():
+        hvConfig["OaiPmhETL.rangeTo"] = config["hvRangeTo"]
+    neutConfig = loadHvConfig(os.path.join(config["hvConfigDir"], "neut.config.json"))
+    # neutralize parameters since Harvester do not care about meaningful order to change params
+    try:
+        r = requests.post(hvUrl, data=json.dumps(neutConfig), headers=headers)
+        logger.debug("Neutralizing difficult hv params: {} {}".format(r.status_code, r.reason))
+    except Exception as e:
+        logger.error(e)
+
     try:
         r = requests.post(hvUrl, data=json.dumps(hvConfig), headers=headers)
-        logger.info("{} {}".format(r.status_code, r.reason))
+        logger.debug("{} {}".format(r.status_code, r.reason))
     except Exception as e:
         logger.error(e)
 
 def checkHarvester(hv):
-    logger.info("Checking harvester {}".format(hv))
+    logger.debug("Checking harvester {}".format(hv))
     hvUrl = hv
     r = requests.get(hvUrl)
-    logger.debug("{} {} {}".format(r.status_code, r.reason, r.text))
+    logger.info("{} {} {}".format(r.status_code, r.reason, r.text))
     hvState = json.loads(r.text)["state"]
-    logger.debug("Harvester state: {}".format(hvState))
-    return hvState
+    hvHealth = json.loads(r.text)["health"]
+    logger.info("Harvester state: {} - health {}".format(hvState, hvHealth))
+    return (hvState, hvHealth)
 
 def startHarvester(hv):
     hvUrl = hv
@@ -87,7 +116,7 @@ def startHarvester(hv):
 
 def unloadHarvester(hvIdx, config, target):
     command =  config["hvUnloadCmd"].format(config["hvUnloadSrc"][hvIdx],target)
-    logger.debug("unload with {}".format(command))
+    logger.debug("unload harvester {} with {}".format(config["hvs"][hvIdx], command))
     subprocess.run(config["hvUnloadCmd"].format(config["hvUnloadSrc"][hvIdx],
                                              target).split())
 
